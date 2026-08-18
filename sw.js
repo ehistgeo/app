d5c05558051371b61174b8dbc6389fb8c57f1341
ehistgeo <e.histgeo@gmail.com>
2026-08-18 17:03:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // L'app s'ouvre donc instantanément et hors ligne, et se met à jour à la visite suivante.
 // Incrémenter CACHE à chaque changement de la liste ASSETS.
 
-const CACHE = 'ehistgeo-v6';
+const CACHE = 'ehistgeo-v7';
 
 const ASSETS = [
   './',
